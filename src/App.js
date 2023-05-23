f1e4714d8bea1d0fbcde0945210f2d5aa5b25b80
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-05-22 22:21:56 -0700

    updated css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import { BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
+import { Link } from "react-router-dom";
+import logo from "./img/logo.png";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Link className="nav" to={"/"}>
+          <img src={logo} className="logo" />
+          <h4>Foodie</h4>
+        </Link>
         <Search />
+
         <Category />
         <Pages />
       </BrowserRouter>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import Veggie from "../components/Veggie";
 import Popular from "../components/Popular";
+import Desert from "../components/Desert";
 
 import React from 'react'
 
@@ -8,6 +9,7 @@ function Home() {
     <div>
       <Veggie />
       <Popular />
+      <Desert />
     </div>
   )
 }
